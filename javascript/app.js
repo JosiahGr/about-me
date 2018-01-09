ccacae7712db15c195ae0d83a7a4ceac47a4c362
Josiah Green <JosiahGreen90@gmail.com>
2018-01-09 14:38:21 -0800

    Added working Y-N Question 1

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,18 @@
-alert ('testing 1 2 3');
+'use strict';
+
+alert ('Hello and welcome.');
+
+var userName = prompt ('What\'s your name?');
+alert('Hello ' + userName);
+console.log('The user entered: ' + userName);
+
+var myAge = prompt ('Do you think I\'m older than 25? Answer with Y or N.').toUpperCase();
+
+if(myAge === 'Y') {
+    alert('Yeah! You got it!');
+} else {
+    alert('Nope. You\'re very wrong. But that\'s okay!');
+}
+
+
+
