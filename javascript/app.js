cf8a61887f2116ddcc86a97ed531244851c862c4
Josiah Green <JosiahGreen90@gmail.com>
2018-01-10 12:57:13 -0800

    Fixed my wording and console log errors

--- javascript/app.js
+++ javascript/app.js
@@ -3,9 +3,9 @@
 alert ('Hello and welcome.');
 
 var userName = prompt ('What\'s your name?');
-alert('Thanks for stopping by ' + userName + '. I have some questions for you to answer since you\'re here.');
-console.log('The users name is: ' + userName);
 
+alert('Thanks for stopping by ' + userName + '. let\s play a game!');
+console.log('The users name is: ' + userName);
 
 // // Question 1
 
@@ -14,9 +14,9 @@ var myAge = prompt ('Do you think I\'m older than 25? Answer with Y or N.').toUp
 if(myAge === 'Y' || myAge === 'YES') {
     alert('Yeah! You got it!');
 } else {
-    alert('Nope. You\'re very wrong. But that\'s okay!');
+    alert('Nope. You\'re wrong. But that\'s okay!');
 }
-console.log('The user guessed: ' + myAge);
+console.log('Q1 The user guessed: ' + myAge);
 
 // // Question 2
 
@@ -27,30 +27,30 @@ if (seattleNative === 'N' || seattleNative === 'NO') {
 } else {
     alert ('Nope. I\'m actually from Denver, Colorado.');
 }
-console.log('The user guessed: ' + seattleNative);
+console.log('Q2 The user guessed: ' + seattleNative);
 
 // Question 3
 
 var cheese = prompt ('Do you think I like cheese? Answer Y or N.').toUpperCase();
 
 if(cheese === 'Y' || cheese === 'YES') {
-    alert ('NO! I hate cheese. I really don\'t understand why its a thing. Like wtf is Blue Cheese all about?')
+    alert ('NO! I hate cheese. I really don\'t understand why its a thing.')
 } else {
     alert ('CORRECT! It\'s pretty nasty. I don\'t understand why it\'s a thing.')
 }
-console.log('The user guessed: ' + cheese);
+console.log('Q3 The user guessed: ' + cheese);
 
 // Question 4
 
-var inanimateObject = prompt ('If I could eliminate one inanimate object from existence, what would I choose? Type 1 for Selfie Sticks or 2 for Blue Cheese.').toUpperCase;
+var inanimateObject = prompt ('If I could eliminate one inanimate object from existence, what would I choose? Type 1 for Selfie Sticks or 2 for Blue Cheese.').toUpperCase();
 
-if (inanimateObject === '1' || inanimateObject === 'ONE') {
-    alert ('YES! They are so dumb. Yeah I\'m talking to all of you who use them. You look ridiculous.');
+if(inanimateObject === '1' || inanimateObject === 'ONE') {
+    alert ('YES! They are so ridiculous looking.');
 } else { 
     alert ('Wrong. While I think Blue Cheese is horrible, I can keep my distance. I truly wish selfie sticks weren\'t an actual thing.');
 }
 
-console.log('The user guessed: ' + inanimateObject);
+console.log('Q4 The user guessed: ' + inanimateObject);
 
 // Question 5 
 
@@ -64,6 +64,6 @@ if (howAmIDoing === 'Y' || howAmIDoing === 'YES') {
     alert ('Hey, thanks! You\'re too kind. Your compliment is going right up on my fridge.');
 }
 
-console.log('The user guessed: ' + inanimateObject);
+console.log('Q5 The user guessed: ' + howAmIDoing);
 
 alert ('Thanks ' + userName + ' for your time! See you soon.');
